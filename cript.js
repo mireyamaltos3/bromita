19081298f37d121ef1fd3302c8ca604aab12f53d
mireyamaltos3 <158123365+mireyamaltos3@users.noreply.github.com>
2026-07-08 18:12:08 -0600

    Display image after 2.5 seconds delay
    Add a timeout to display an image after 2.5 seconds.

---
+++ cript.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+
+    document.body.innerHTML = `
+        <img src="imagen.png" alt="Imagen" style="
+            width: 100vw;
+            height: 100vh;
+            object-fit: cover;
+        ">
+    `;
+
+}, 2500);
